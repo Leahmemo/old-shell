f03ce8718372e5c7f784e2306d8e431e43db2763
JoanWaithira <joanwaithira.jw@gmail.com>
2023-06-19 06:52:47 -0700

    End of Day Monday

--- main.h
+++ main.h
@@ -1,10 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
+#define MAX_COMMAND_LENGTH 100
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <stddef.h>
 #include <fcntl.h>
--- shell.c
+++ shell.c
@@ -14,6 +14,22 @@ int main(void)
 	char *argv[] = {"bin/ls", "-l", "/usr/", NULL};
 	pid_t child_pid = getpid();
 	int status;
+	char command[MAX_COMMAND_LENGTH];
+
+	while (1)
+	{
+		printf("$shell> ");
+
+		fgets(command, sizeof(command), stdin);
+
+		command[strcspn(command, "\n")] = '\0';
+
+		if (strcmp(command, "exit") == 0)
+		{
+			printf("You are leaving the shell ....\n");
+			break;
+		}
+
 
 
 	printf("Before execve\n");
@@ -26,11 +42,11 @@ int main(void)
 		 perror ("Error: \n");
                  return (1);
          }
-         if (child_pid == 0)
+	 if (child_pid == 0)
          {
 		 printf("Waiting....\n");
 		 printf("Executing\n");
-		 execve("/bin/ls", argv, NULL);
+		execve("/bin/ls", argv, NULL) == 1;
 		 perror("Error: \n");
                  sleep(3);
          }
@@ -42,5 +58,7 @@ int main(void)
 
 	  printf("After execve\n");
 	  return (0);
+	}
+	printf("Still in shell");
+	return (0);
 }
-
