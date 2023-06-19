b9628378151ecce24b1750d10aec658b0655f239
JoanWaithira <joanwaithira.jw@gmail.com>
2023-06-19 06:12:47 -0700

    Adding the header file

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+
+
+#endif
+
