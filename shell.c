ced3c6dbae1cb088b6d5373f41216a30967bd278
JoanWaithira <joanwaithira.jw@gmail.com>
2023-06-19 23:52:33 -0700

    Simple Simple Shell

--- shell.c
+++ shell.c
@@ -2,10 +2,6 @@
 
 /**
  * main - our version of a simple shell
- *
- * @argc: argument count
- * @argv: argument vector
- *
  * Return: Hopefully a well working shell
  */
 
@@ -19,45 +15,33 @@ int main(void)
 	while (1)
 	{
 		printf("$shell> ");
-
 		fgets(command, sizeof(command), stdin);
-
 		command[strcspn(command, "\n")] = '\0';
-
 		if (strcmp(command, "exit") == 0)
 		{
 			printf("You are leaving the shell ....\n");
 			break;
 		}
-
-
-
-	printf("Before execve\n");
-
-	 printf("Ready to fork...\n");
-        child_pid = fork();
-
-         if (child_pid == -1)
-         {
-		 perror ("Error: \n");
-                 return (1);
-         }
-	 if (child_pid == 0)
-         {
-		 printf("Waiting....\n");
-		 printf("Executing\n");
-		execve("/bin/ls", argv, NULL) == 1;
-		 perror("Error: \n");
-                 sleep(3);
-         }
-         else
-          {
-                  wait(&status);
-                  printf("All better now...\n");
-          }
-
-	  printf("After execve\n");
-	  return (0);
+		printf("Before execve\n Ready to fork....\n");
+		child_pid = fork();
+		if (child_pid == -1)
+		{
+			perror("Error: \n");
+			return (1);
+		}
+		if (child_pid == 0)
+		{
+			printf("Waiting....\n Executing\n");
+			execve("/bin/ls", argv, NULL);
+			perror("Error: \n");
+			sleep(3);
+		}
+		else
+		{
+			wait(&status);
+			printf("All better now...\n");
+		}
+		return (0);
 	}
 	printf("Still in shell");
 	return (0);
