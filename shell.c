f89b5ede95b6c334c4f0b8542208c0a2a8cc5946
JoanWaithira <joanwaithira.jw@gmail.com>
2023-06-30 14:51:09 -0700

    system()

--- shell.c
+++ shell.c
@@ -7,8 +7,22 @@
 
 void displayShell(void)
 {
-	printf("Shell $ ");
+	printf("Shell >> ");
 	fflush(stdout);
+}
+
+/**
+ * command_execution -  function that executes a command
+ * @command: The command to be executed
+ */
+
+void command_execution(const char *command)
+{
+	int response = system(command);
+	if (response == -1)
+	{
+		perror("Sorry, does not recognise command");
+	}
 }
 
 
@@ -16,84 +30,28 @@ void displayShell(void)
  * main - A function that returns our version of a simple shell
  * Return: Hopefully a well working shell
  */
-
 int main(void)
 {
-	pid_t child_pid;
-	int status;
 	char *command = NULL;
-	const char *delim = " \n";
 	size_t n = 0;
-	/*char *cmd[] = {"bin/sh", "-c", NULL, NULL};*/
-	char **argv = NULL;
-	int a, b;
-	char *token;
-	char *tokenised_command;
-	int total_tokens = 0;i
 
-	while (1)
-	{
-		displayShell();
-	if (getline(&command, &n, stdin) == -1)
-	{
-		perror("Exiting shell ");
+        while (1)
+        {
+                displayShell();
+        if (getline(&command, &n, stdin) == -1)
+        {
+                perror("Exiting shell ");
+                break;
+        }
+        command[strcspn(command, "\n")] = '\0';
+
+        if (strcmp(command, "exit") == 0)
+        {
+                printf("You are leaving the shell...Sad to see you go...\n");
 		break;
 	}
-	command[strcspn(command, "\n")] = '\0'; 
-
-	if (strcmp(command, "exit") == 0)
-	{
-		printf("You are leaving the shell...Sad to see you go...\n");
-		break;
+	command_execution(command);
 	}
-
-	tokenised_command = strdup(command);
-
-	token = strtok(tokenised_command, delim);
-
-	while (token)
-	{
-		total_tokens++;
-		token = strtok (NULL, delim);
-	}
-
-	argv = malloc((total_tokens + 2) * sizeof(char*));
-
-	a = 0;
-	token = strtok(command, delim);
-	while (token)
-	{
-		argv[a] = strdup(token);
-		token = strtok(NULL, delim);
-		a++;
-	}
-	argv[a] = NULL;
-
-	free(tokenised_command);
-	child_pid = fork();
-	if (child_pid == -1)
-	{
-		perror("Error: ");
-		exit(1);
-	}
-	if (child_pid == 0)
-	{
-		argv[0] = "bin/sh";
-		execve(argv[0], argv, NULL);
-		perror("Error: ");
-		exit(1);
-	}
-	else
-	{
-		waitpid(child_pid, &status, 0);
-	}
-	
-	for(b = 0; b < a; b++)
-	{
-		free(argv[b]);
-	}
-	}
-	free(argv);
 	free(command);
 	return (0);
 }
