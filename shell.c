b3162e09d9b6c00be2da215c8305ba7818a32d95
JoanWaithira <joanwaithira.jw@gmail.com>
2023-06-30 14:55:09 -0700

    BAck to initial

--- shell.c
+++ shell.c
@@ -10,30 +10,24 @@ void displayShell(void)
 	printf("Shell >> ");
 	fflush(stdout);
 }
-
-/**
- * command_execution -  function that executes a command
- * @command: The command to be executed
- */
-
-void command_execution(const char *command)
-{
-	int response = system(command);
-	if (response == -1)
-	{
-		perror("Sorry, does not recognise command");
-	}
-}
-
-
 /**
  * main - A function that returns our version of a simple shell
  * Return: Hopefully a well working shell
  */
 int main(void)
 {
-	char *command = NULL;
-	size_t n = 0;
+        pid_t child_pid;
+        int status;
+        char *command = NULL;
+        const char *delim = " \n";
+        size_t n = 0;
+        /*char *cmd[] = {"bin/sh", "-c", NULL, NULL};*/
+        char **argv = NULL;
+        int a, b;
+        char *token;
+        char *tokenised_command;
+        char *custom_path = "/bin:/usr/bin";
+        int total_tokens = 0;
 
         while (1)
         {
@@ -48,10 +42,57 @@ int main(void)
         if (strcmp(command, "exit") == 0)
         {
                 printf("You are leaving the shell...Sad to see you go...\n");
-		break;
-	}
-	command_execution(command);
-	}
-	free(command);
-	return (0);
+                break;
+        }
+
+        tokenised_command = strdup(command);
+
+        token = strtok(tokenised_command, delim);
+
+        while (token)
+        {
+                total_tokens++;
+                token = strtok (NULL, delim);
+        }
+
+        argv = malloc((total_tokens + 2) * sizeof(char*));
+
+        a = 0;
+        token = strtok(command, delim);
+        while (token)
+        {
+                argv[a] = strdup(token);
+                token = strtok(NULL, delim);
+                a++;
+        }
+        argv[a] = NULL;
+
+        free(tokenised_command);
+        child_pid = fork();
+        if (child_pid == -1)
+        {
+                perror("Error: ");
+                exit(1);
+        }
+        if (child_pid == 0)
+        {
+                /*setenv("PATH", custom_path, 1);*/
+               /* argv[0] = "sh";*/
+                execlp("/bin/sh", "sh", "-c",command, NULL);
+                perror("Error: ");
+                exit(1);
+        }
+        else
+        {
+                waitpid(child_pid, &status, 0);
+        }
+
+        for(b = 0; b < a; b++)
+        {
+                free(argv[b]);
+        }
+        free(argv);
+        free(command);
+        }
+        return (0);
 }
